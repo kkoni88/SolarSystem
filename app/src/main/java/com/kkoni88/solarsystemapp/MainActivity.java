94476beb53a2431af44d11499272b84a3b1637d9
kkoni88 <jzz30man@gmail.com>
2019-07-23 17:50:52 +0200

    poc

--- app/src/main/java/com/kkoni88/solarsystemapp/MainActivity.java
+++ app/src/main/java/com/kkoni88/solarsystemapp/MainActivity.java
@@ -1,16 +1,17 @@
 package com.kkoni88.solarsystemapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import okhttp3.HttpUrl;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,29 +22,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView statusTV = (TextView) findViewById(R.id.status);
-        String apiKey = getResources().getString(R.string.solar_api_key);
-        String siteId = getResources().getString(R.string.solar_site_id);
+        final TextView statusTV = findViewById(R.id.status);
 
-        String siteDetailsUrl =
-                String.format("https://monitoringapi.solaredge.com/site/%s/details?api_key=%s", siteId, apiKey);
-        String sitePowerBulk = getResources().getString(R.string.solar_sitePowerBulk);
+        String url = new HttpUrl.Builder()
+                .scheme(getResources().getString(R.string.solar_monitoring_api_scheme))
+                .host(getResources().getString(R.string.solar_monitoring_api_host))
+                .addPathSegments(getResources().getString(R.string.solar_monitoring_api_path_site))
+                .addPathSegment("energyDetails")
+                .addQueryParameter("api_key", getResources().getString(R.string.solar_api_key))
+                .addQueryParameter("startTime", "2019-07-23 00:00:00")
+                .addQueryParameter("endTime", "2019-07-24 00:00:00")
+                .build().toString();
+
 
 
         Runnable runnable = () -> {
             RequestQueue queue = Volley.newRequestQueue(this);
-            StringRequest stringRequest = new StringRequest(Request.Method.GET, sitePowerBulk,
-                    new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            statusTV.setText(response);
-                        }
-                    }, new Response.ErrorListener() {
 
-                @Override
-                public void onErrorResponse(VolleyError error) {
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    (response) -> {
+                        JsonObject jsonObject = new JsonParser().parse(response).getAsJsonObject();
+                        String generatedPower =
+                                jsonObject.get("energyDetails").getAsJsonObject().get("meters").getAsJsonArray().get(0).getAsJsonObject().get("values").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsString();
+
+                        statusTV.setText(String.format("A mai megtermelt áram: %s Wh", generatedPower));
+
+                    }, (error) -> {
                     statusTV.setText("That didn't work!");
-                }
+
             });
 
             // Add the request to the RequestQueue.
