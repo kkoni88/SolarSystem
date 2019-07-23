451af17a6f205cc21488370fc387c244033e3e44
kkoni88 <jzz30man@gmail.com>
2019-07-23 22:21:27 +0200

    added styling

--- app/src/main/java/com/kkoni88/solarsystemapp/MainActivity.java
+++ app/src/main/java/com/kkoni88/solarsystemapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kkoni88.solarsystemapp;
 
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -10,6 +11,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import okhttp3.HttpUrl;
 
@@ -24,16 +28,17 @@ public class MainActivity extends AppCompatActivity {
 
         final TextView statusTV = findViewById(R.id.status);
 
+        String todayStr = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+
         String url = new HttpUrl.Builder()
                 .scheme(getResources().getString(R.string.solar_monitoring_api_scheme))
                 .host(getResources().getString(R.string.solar_monitoring_api_host))
                 .addPathSegments(getResources().getString(R.string.solar_monitoring_api_path_site))
                 .addPathSegment("energyDetails")
                 .addQueryParameter("api_key", getResources().getString(R.string.solar_api_key))
-                .addQueryParameter("startTime", "2019-07-23 00:00:00")
-                .addQueryParameter("endTime", "2019-07-24 00:00:00")
+                .addQueryParameter("startTime", String.format("%s 00:00:00", todayStr))
+                .addQueryParameter("endTime", String.format("%s 23:59:59", todayStr))
                 .build().toString();
-
 
 
         Runnable runnable = () -> {
@@ -45,14 +50,12 @@ public class MainActivity extends AppCompatActivity {
                         String generatedPower =
                                 jsonObject.get("energyDetails").getAsJsonObject().get("meters").getAsJsonArray().get(0).getAsJsonObject().get("values").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsString();
 
-                        statusTV.setText(String.format("A mai megtermelt áram: %s Wh", generatedPower));
+                        statusTV.setText(String.format("A ma megtermelt áram: %s Wh", generatedPower));
 
                     }, (error) -> {
-                    statusTV.setText("That didn't work!");
+                statusTV.setText("That didn't work!");
 
             });
-
-            // Add the request to the RequestQueue.
             queue.add(stringRequest);
         };
 
